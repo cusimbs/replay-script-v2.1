b2b04f2fb11d9409222dfbb1b7f8e809f7c17d13
cusimbs <67462039+cusimbs@users.noreply.github.com>
2020-09-04 09:23:25 +0200

    Delete get-replay-data.sh

--- get-replay-data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-mkdir -p logfiles
-#docker run --rm --name replay-data  -v logfiles:/logfiles replay-data python3 ../replay-data.py "$@"
-docker start replay-data
-echo "Procesing files"
-docker exec -w /logfiles replay-data python3 /replay-data.py "$@"
-docker stop replay-data
